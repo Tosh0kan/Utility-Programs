ad19157642b7228085f3ddae0657fbfd2ac90861
Tosh0kan <toteshokan@gmail.com>
2024-06-03 21:09:33 -0300

    added inner func and dir/file detect sys

--- imgconv.py
+++ imgconv.py
@@ -1,18 +1,30 @@
+import os
 import argparse
 import pillow_avif
+from pprint import pp
 from PIL import Image
+from pathlib import Path
 
-def img_convert(i_file: str, fmt: str) -> Image:
-    icon = Image.open(r"{}".format(i_file)).convert("RGB")
+def img_convert(path_outer: str, fmt: str) -> Image:
+    def file_convert(path_inner: str, fmt: str) -> Image:
+        icon = Image.open(r"{}".format(path_inner)).convert("RGB")
+        icon.save(r"{}.{}".format(path_inner.split('.')[0], fmt), format=f"{fmt.upper()}")
 
-    icon.save(r"{}.{}".format(i_file.split('.')[0], fmt), format=f"{fmt.upper()}")
+    print(path_outer)
+    if Path(path_outer).is_file():
+        file_convert(path_outer, fmt)
+
+    elif Path(path_outer).is_dir():
+        dir_ls = os.listdir(path_outer)
+        file_list = [path_outer + '\\' + e for e in dir_ls]
+        pp(file_list)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", help="Input file path")
-    parser.add_argument("-f", help="Output file format")
+    parser.add_argument("-p", help="Input file or folder path", default=os.getcwd())
+    parser.add_argument("-f", help="Output file/files format")
     args = parser.parse_args()
-    img_convert(args.i, args.f)
+    img_convert(args.p, args.f)
 
 
 if __name__ == "__main__":
