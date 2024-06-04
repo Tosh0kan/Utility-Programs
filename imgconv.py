af5449cc59d84b1360e0e772d2d5d9b5fdbce8e2
Tosh0kan <toteshokan@gmail.com>
2024-06-04 13:49:49 -0300

    Removed redundancies

--- imgconv.py
+++ imgconv.py
@@ -8,16 +8,12 @@ from datetime import datetime as dt
 
 
 async def img_convert(path_outer: str, fmt: str) -> Image:
-    def file_convert(count, path_inner: str, fmt: str, batch: bool = False) -> Image:
-        if not batch:
-            icon = Image.open(r"{}".format(path_inner)).convert("RGB")
-            icon.save(r"{}.{}".format(path_inner.split('.')[0], fmt), format=f"{fmt.upper()}")
-        elif batch:
-            converted_folder = '\\'.join(path_inner.split('\\')[0:-1]) + '\\converted'
-            img_name = path_inner.split('\\')[-1].split('.')[0]
-            icon = Image.open(r"{}".format(path_inner)).convert("RGB")
-            icon.save(r"{}.{}".format(converted_folder  + '\\' + img_name, fmt), format=f"{fmt.upper()}")
-            print(f"Saved file {count+1}/{len(file_list)}")
+    def file_convert(count, path_inner: str, fmt: str) -> Image:
+        converted_folder = '\\'.join(path_inner.split('\\')[0:-1]) + '\\converted'
+        img_name = path_inner.split('\\')[-1].split('.')[0]
+        icon = Image.open(r"{}".format(path_inner)).convert("RGB")
+        icon.save(r"{}.{}".format(converted_folder  + '\\' + img_name, fmt), format=f"{fmt.upper()}")
+        print(f"Saved file {count}/{len(file_list)}")
 
 
     if Path(path_outer).is_file():
@@ -34,8 +30,8 @@ async def img_convert(path_outer: str, fmt: str) -> Image:
         except FileExistsError:
             pass
         start = dt.now()
-        await asyncio.gather(*(asyncio.to_thread(file_convert, i, e, 'png', batch=True)
-                               for i, e in enumerate(file_list)))
+        await asyncio.gather(*(asyncio.to_thread(file_convert, i, e, 'png')
+                               for i, e in enumerate(file_list, start=1)))
         print(f"Total time to convert: {dt.now() - start}")
 
 
