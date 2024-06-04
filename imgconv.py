c31ac69a4c7ec51bd3aaf8d5ce7bde80cf7435cf
Tosh0kan <toteshokan@gmail.com>
2024-06-04 10:10:57 -0300

    Added UX ammenities to async workflow

--- imgconv.py
+++ imgconv.py
@@ -6,8 +6,9 @@ from PIL import Image
 from pathlib import Path
 from datetime import datetime as dt
 
+
 async def img_convert(path_outer: str, fmt: str) -> Image:
-    def file_convert(path_inner: str, fmt: str, batch: bool = False) -> Image:
+    def file_convert(count, path_inner: str, fmt: str, batch: bool = False) -> Image:
         if not batch:
             icon = Image.open(r"{}".format(path_inner)).convert("RGB")
             icon.save(r"{}.{}".format(path_inner.split('.')[0], fmt), format=f"{fmt.upper()}")
@@ -16,21 +17,28 @@ async def img_convert(path_outer: str, fmt: str) -> Image:
             img_name = path_inner.split('\\')[-1].split('.')[0]
             icon = Image.open(r"{}".format(path_inner)).convert("RGB")
             icon.save(r"{}.{}".format(converted_folder  + '\\' + img_name, fmt), format=f"{fmt.upper()}")
+            print(f"Saved file {count+1}/{len(file_list)}")
+
 
     if Path(path_outer).is_file():
         file_convert(path_outer, fmt)
 
     elif Path(path_outer).is_dir():
         dir_ls = os.listdir(path_outer)
+
+        global file_list
         file_list = [path_outer + '\\' + e for e in dir_ls]
+
         try:
             os.mkdir(path_outer + '\\' + 'converted')
         except FileExistsError:
             pass
         start = dt.now()
-        # tasks = (file_convert(e, 'png', batch=True) for e in file_list)
-        await asyncio.gather(*(asyncio.to_thread(file_convert, e, 'png', batch=True) for e in file_list))
+        await asyncio.gather(*(asyncio.to_thread(file_convert, i, e, 'png', batch=True)
+                               for i, e in enumerate(file_list)))
         print(f"Total time to convert: {dt.now() - start}")
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", help="Input file or folder path", default=os.getcwd())
