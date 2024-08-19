f6314e9d467732d1c0cd57eae94c9560d47692fe
Tosh0kan <toteshokan@gmail.com>
2024-08-19 12:27:20 -0300

    refact string insertion logic into sub func

--- get_all_literotica.py
+++ get_all_literotica.py
@@ -42,6 +42,62 @@ async def scrape_and_proc(url: str, path: str, custom_title: str = None,
 
         return ''.join(char_list)
 
+    def string_insertion(og_title: str, prefix: str = None, suffix: str = None,
+                         custom_title: str = None, title_add: list[str] = None) -> str:
+        if custom_title is not None:
+            og_title = custom_title
+        elif prefix is not None and suffix is not None:
+            og_title = prefix + ' ' + og_title + ' ' + suffix
+        elif prefix is not None:
+            og_title = prefix + ' ' + og_title
+        elif suffix is not None:
+            og_title = og_title + ' ' + suffix
+        elif title_add is not None:
+            global usage_info
+            sub_chars = ["{", "}"]
+            padding_chars = ("<", "^", ">")
+            if len(title_add) < 3:
+                raise Exception("There's not engough parameters or too many for title addition."
+                                "\nThere are exaclty three, they must be in this order, and of these types:"
+                                f"{usage_info}")
+            elif len(title_add) == 3:
+                if title_add[1] not in padding_chars:
+                    raise Exception("Invalid alignment option. Valid alignment options are:"
+                                    "\n\t\t<: Left aglignment. Inserts to the left of the inserted text."
+                                    "\n\t\t^: Center Alignment. Inserts to the left and right of the inserted text."
+                                    "\n\t\t>: Right alignment. Inserts to the right of the inserted text.")
+                else:
+                    if title_add[0].isdigit():
+                        idx_pos = title_add[0]
+                    else:
+                        if len(title_add[0]) == 1:
+                            idx_pos = og_title.index(title_add[0])
+                        else:
+                            idx_pos = og_title.index(title_add[0]) + len(title_add[0])
+
+                    alignment = title_add[1]
+                    to_insert = title_add[2]
+
+            if alignment == '^':
+                form_syntax = list(f":{alignment}{len(to_insert)+2}")
+                for e in reversed(form_syntax):
+                    sub_chars.insert(1, e)
+            else:
+                form_syntax = list(f":{alignment}{len(to_insert)+1}")
+                for e in reversed(form_syntax):
+                    sub_chars.insert(1, e)
+
+            story_title_list = list(og_title)
+
+            for e in reversed(sub_chars):
+                story_title_list.insert(int(idx_pos), e)
+
+            story_title = ''.join(story_title_list).format(to_insert)
+        else:
+            pass
+        return story_title
+
+
     base_url = url
 
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0'}
@@ -69,58 +125,8 @@ async def scrape_and_proc(url: str, path: str, custom_title: str = None,
     pages_texts = [bs(page.text, 'lxml').find('div', class_='aa_ht').prettify() for page in reqs]
     text_body = ''.join(pages_texts)
 
-
-    if custom_title is not None:
-        story_title = custom_title
-    elif prefix is not None and suffix is not None:
-        story_title = prefix + ' ' + story_title + ' ' + suffix
-    elif prefix is not None:
-        story_title = prefix + ' ' + story_title
-    elif suffix is not None:
-        story_title = story_title + ' ' + suffix
-    elif title_add is not None:
-        global usage_info
-        sub_chars = ["{", "}"]
-        padding_chars = ("<", "^", ">")
-        if len(title_add) < 3:
-            raise Exception("There's not engough parameters or too many for title addition."
-                            "\nThere are exaclty three, they must be in this order, and of these types:"
-                            f"{usage_info}")
-        elif len(title_add) == 3:
-            if title_add[1] not in padding_chars:
-                raise Exception("Invalid alignment option. Valid alignment options are:"
-                                "\n\t\t<: Left aglignment. Inserts to the left of the inserted text."
-                                "\n\t\t^: Center Alignment. Inserts to the left and right of the inserted text."
-                                "\n\t\t>: Right alignment. Inserts to the right of the inserted text.")
-            else:
-                if title_add[0].isdigit():
-                    idx_pos = title_add[0]
-                else:
-                    if len(title_add[0]) == 1:
-                        idx_pos = story_title.index(title_add[0])
-                    else:
-                        idx_pos = story_title.index(title_add[0]) + len(title_add[0])
-
-                alignment = title_add[1]
-                to_insert = title_add[2]
-
-        if alignment == '^':
-            form_syntax = list(f":{alignment}{len(to_insert)+2}")
-            for e in reversed(form_syntax):
-                sub_chars.insert(1, e)
-        else:
-            form_syntax = list(f":{alignment}{len(to_insert)+1}")
-            for e in reversed(form_syntax):
-                sub_chars.insert(1, e)
-
-        story_title_list = list(story_title)
-
-        for e in reversed(sub_chars):
-            story_title_list.insert(int(idx_pos), e)
-
-        story_title = ''.join(story_title_list).format(to_insert)
-    else:
-        pass
+    story_title = string_insertion(story_title, prefix=prefix, suffix=suffix,
+                                   custom_title=custom_title, title_add=title_add)
 
     with open(rf'{path}/{story_title}.html', 'w', encoding='utf-8') as f:
         f.write(text_body)
@@ -134,7 +140,8 @@ def main() -> None:
     \n
     \t\t Index or String: The index at which the text will be inserted. First character of the text is index 0.
     \t\tEverything originally at that index and after will be moved to the right.
-    \t\tAlternatively, you can input a string, after which the text to be inserted will be placed.
+    \t\tAlternatively, you can input a string, after which the text to be inserted
+    \t\twill be placed. This is case sensitive.
     \n
     \t\t<: Left aglignment. Inserts space to the right of the inserted text.
     \t\t^: Center Alignment. Inserts space both to the left and right of the inserted text.
