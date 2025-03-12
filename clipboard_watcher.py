20be80cc5fbe9cca4630fff2ff82cc3e1bf9aa2e
Tosh0kan <toteshokan@gmail.com>
2025-03-11 21:56:56 -0300

    better structure of the wrapper

--- clipboard_watcher.py
+++ clipboard_watcher.py
@@ -1,25 +1,25 @@
-import pyperclip
 import pynput
+import pyperclip
 from time import sleep
 
 
-def clipboard_proccer():
-    sleep(0.1)
-    return _clipboard_proccer()
+def wrapper_proccer():
+    def clipboard_proccer():
+        print('into proccer')
+        current_value = pyperclip.paste()
+        current_value = current_value.replace('\n','')
+        current_value = current_value.replace('\r', '')
+        pyperclip.copy(current_value)
+        print('out of proccer')
 
-def _clipboard_proccer():
-    print('into proccer')
-    current_value = pyperclip.paste()
-    current_value = current_value.replace('\n','')
-    current_value = current_value.replace('\r', '')
-    pyperclip.copy(current_value)
-    print('out of proccer')
+    sleep(0.01)
+    return clipboard_proccer()
 
 def for_canonical(f):
     return lambda k: f(listener.canonical(k))
-
+''
 if __name__ == '__main__':
-    hotkey = pynput.keyboard.HotKey(pynput.keyboard.HotKey.parse('<ctrl>+c'), clipboard_proccer)
+    hotkey = pynput.keyboard.HotKey(pynput.keyboard.HotKey.parse('<ctrl>+c'), wrapper_proccer)
     with pynput.keyboard.Listener(on_press=for_canonical(hotkey.press),
                                   on_release=for_canonical(hotkey.release)) as listener:
         listener.join()
