e84433d90e3fb77947e4ee0a450e2cced12ee150
Tosh0kan <toteshokan@gmail.com>
2025-09-16 16:17:29 -0300

    prototype made; no tests

--- clipboard_watcher.py
+++ clipboard_watcher.py
@@ -1,16 +1,45 @@
 import pynput
 import pyperclip
+import argparse
 from time import sleep
 
 
 def wrapper_proccer():
     def clipboard_proccer():
-        if pyperclip.paste() != '':
-            current_value = pyperclip.paste()
-            current_value = current_value.replace('\n', '').replace('\r', '').replace(' ', '')
-            pyperclip.copy(current_value)
-        else:
-            pass
+        if args.averager:
+            freq_check = False
+
+            s = pyperclip.paste()
+            chrs = ([chr(e) for e in range(0x3041, 0x3097)] +
+                    [chr(e) for e in range(0x30a1, 0x30fb)] +
+                    [chr(e) for e in range(0x4e00, 0x9fb0)])
+
+            matches = {True: 0, False: 0}
+            for e in s:
+                if e in chrs:
+                    matches[True] += 1
+                else:
+                    matches[False] += 1
+            if matches[True] >= (len(s) / 5) * 4:
+                freq_check = True
+            else:
+                pass
+
+            if freq_check:
+                if pyperclip.paste() != '':
+                    current_value = pyperclip.paste()
+                    current_value = current_value.replace('\n', '').replace('\r', '').replace(' ', '')
+                    pyperclip.copy(current_value)
+                else:
+                    pass
+            else:
+                if pyperclip.paste() != '':
+                    current_value = pyperclip.paste()
+                    current_value = current_value.replace('\n', '').replace('\r', '')
+                    pyperclip.copy(current_value)
+                else:
+                    pass
+                
 
     sleep(0.01)
     return clipboard_proccer()
@@ -28,7 +57,14 @@ def on_release(key):
     for hotkey in hotkeys:
         hotkey.release(canonical_key)
 
+
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--averager', action='store_true')
+    args = parser.parse_args()
+
     hotkeys = [
         pynput.keyboard.HotKey(pynput.keyboard.HotKey.parse('<ctrl>+c'), wrapper_proccer),
         pynput.keyboard.HotKey(pynput.keyboard.HotKey.parse('<ctrl>+q'), quit_process),
