a9f467e21da605566beaca4318ba30b466dbd3ab
Tosh0kan <toteshokan@gmail.com>
2025-10-09 12:15:01 -0300

    minor tweak

--- csp_start.py
+++ csp_start.py
@@ -11,6 +11,8 @@ def start_csp() -> None:
     sleep(2)
     pyautogui.write('lai2 zi4 bbs.itzmx.com mian3 fei4 fen1 xiang3 fa1 xian4 fan4 mai4 cha4 ping2 ju3 bao4 tui4 kuan3 '+
                    'bbs.itzmx.com Always Free')
+    sleep(0.5)
+    pyautogui.press('enter')
 
 if __name__ == '__main__':
     start_csp()
