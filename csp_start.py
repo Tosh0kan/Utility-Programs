646a608de975209b72f9c6585618383be7691ec9
Tosh0kan <toteshokan@gmail.com>
2025-10-09 12:07:39 -0300

    working csp starter

--- csp_start.py
+++ csp_start.py
@@ -1,5 +1,4 @@
 import os
-import pyperclip
 import pyautogui
 from time import sleep
 
@@ -7,12 +6,11 @@ from time import sleep
 def start_csp() -> None:
     os.startfile(r"C:\Program Files\CELSYS\CLIP STUDIO 1.5\CLIP STUDIO PAINT\CLIPStudioPaint.exe")
 
-    pyperclip.copy('lai2 zi4 bbs.itzmx.com mian3 fei4 fen1 xiang3 fa1 xian4 fan4 mai4 cha4 ping2 ju3 bao4 tui4 kuan3 '+
+    print('start')
+    print('text copied')
+    sleep(2)
+    pyautogui.write('lai2 zi4 bbs.itzmx.com mian3 fei4 fen1 xiang3 fa1 xian4 fan4 mai4 cha4 ping2 ju3 bao4 tui4 kuan3 '+
                    'bbs.itzmx.com Always Free')
-    sleep(1)
-    pyautogui.hotkey('ctrl', 'v')
-    sleep(0.55)
-    pyautogui.press('enter')
 
 if __name__ == '__main__':
     start_csp()
