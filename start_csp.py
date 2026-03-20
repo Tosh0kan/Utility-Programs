d995cdaaf1bcd30ac299bb1ead0418e8d314d12b
Tosh0kan <toteshokan@gmail.com>
2026-03-20 20:43:12 -0300

    better window refocusing

--- start_csp.py
+++ start_csp.py
@@ -2,6 +2,8 @@ import os
 import sys
 import ctypes
 import pyautogui
+import win32gui
+import pyperclip
 from time import sleep
 
 def is_admin() -> bool:
@@ -21,14 +23,29 @@ def run_as_adm() -> bool:
         print(f"Failed to elevate privileges: {e}")
         return False
 
+def win_enum_handler(hwnd, top_windows: list):
+    top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
+
 def start_csp() -> None:
     os.startfile(r"C:\Program Files\CELSYS\CLIP STUDIO 1.5\CLIP STUDIO PAINT\CLIPStudioPaint.exe")
 
-    print('start')
-    print('text copied')
-    sleep(2)
-    pyautogui.write('lai2 zi4 bbs.itzmx.com mian3 fei4 fen1 xiang3 fa1 xian4 fan4 mai4 cha4 ping2 ju3 bao4 tui4 kuan3 '+
+    print('start!')
+
+    pyperclip.copy('lai2 zi4 bbs.itzmx.com mian3 fei4 fen1 xiang3 fa1 xian4 fan4 mai4 tui4 kuan3 ju3 bao4 cha4 ping2 '+
                    'bbs.itzmx.com Always Free')
+
+    sleep(0.5)
+    top_windows: list = []
+    win32gui.EnumWindows(win_enum_handler, top_windows)
+    for i in  top_windows:
+        if "application requires" in i[1].lower():
+            print(i)
+            win32gui.ShowWindow(i[0], 5)
+            win32gui.SetForegroundWindow(i[0])
+            break
+
+    sleep(0.5)
+    pyautogui.hotkey('ctrl', 'v')
     sleep(0.5)
     pyautogui.press('enter')
 
