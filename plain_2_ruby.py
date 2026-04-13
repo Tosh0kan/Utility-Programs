18bf6f2fa818b503cf00c2c2cf992b94fbaee97f
Tosh0kan <toteshokan@gmail.com>
2026-04-12 22:09:42 -0300

    plain2ruby finalized and working

--- plain_2_ruby.py
+++ plain_2_ruby.py
@@ -1,5 +1,35 @@
 import re
+import argparse
+import pyperclip
 
 input_txt = "彼は 政界[せいかい]なのに 有名人[ゆうめいじん]と<b>親[した]しい。</b>"
-split_txt = [e for e in re.split(r'\s+|(\w+\[\w+\])|(\w+)|(<b>.*</b>)', input_txt) if e != '' and e is not None]
-print(split_txt)
+
+def plain2ruby(term_list: str):
+    def reg_htmler(plain_chunk: str) -> str:
+        return '<span class="term">' + plain_chunk + '</span>'
+
+    def furiganed_htmler(furiganed_chunk: str) -> str:
+        hrtmled_furigana = furiganed_chunk.replace('[', '<rt>').replace(']', '</rt>')
+        return '<span class="term"><ruby>' + hrtmled_furigana + '</ruby></span>'
+
+    def bolded_htmler(bolded_chunk: str) -> str:
+        return '<span class="term"><b>' + bolded_chunk + '</b></span>'
+
+    split_txt = [e for e in re.split(r'\s+|(\w+\[\w+\])|(\w+)|(<b>.*</b>)', term_list) if e != '' and e is not None]
+    htmled_list = []
+    for e in split_txt:
+        if '[' in e:
+            htmled_list.append(furiganed_htmler(e))
+        elif '<b>' in e:
+            htmled_list.append(bolded_htmler(e))
+        else:
+            htmled_list.append(reg_htmler(e))
+    return '\n'.join(htmled_list)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('text', type=str, help='plain text')
+    args = parser.parse_args()
+
+    pyperclip.copy(plain2ruby(args.text))
+    print('The converted text was successfuly copied to the clipboard.')
